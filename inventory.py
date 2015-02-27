8d8e272509f3f62abb31ea67a73b662069215b73
Pavel Pulec <kayn@inuits.eu>
2015-02-27 11:09:21 +0100

    handled exception

--- inventory.py
+++ inventory.py
@@ -2,23 +2,25 @@
 
 from pypuppetdb import connect
 import json
-db = connect(host='puppetdb.example.com')
 
-hosts = {}
+try:
+  db = connect(host='puppetdb.example.com')
+  hosts = {}
+  facts = db.facts('environment')
 
-facts = db.facts('environment')
+  for fact in facts:
+    hostname_str = str(fact)
+    hostname = hostname_str[12:]
+    env = str(fact.value)
 
-for fact in facts:
-        hostname_str = str(fact)
-        hostname = hostname_str[12:]
-        env = str(fact.value)
+    if env in hosts:
+            new_list = hosts[env]
+    else:
+            new_list = []
 
-        if env in hosts:
-                new_list = hosts[env]
-        else:
-                new_list = []
+    new_list.append(hostname)
+    hosts[env] = new_list
 
-        new_list.append(hostname)
-        hosts[env] = new_list
-
-print json.dumps(hosts, indent=2)
+  print json.dumps(hosts, indent=2)
+except:
+  print "{}"
